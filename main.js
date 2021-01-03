c9783e5c869fb64f3eca63407a2f0024fe62926f
Marek Zemla <zemla@marksite4u.pl>
2021-01-03 12:04:22 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('pass');
+const div = document.querySelector('.message');
+const password = "user";
+const message = "password accepted"
+input.addEventListener('input', (e)=> {console.log(e.target.value)
+if(password === e.target.value) {
+div.textContent = message;
+e.target.value = "";
+}
+else {
+    div.textContent = "";
+}
+});
